69c63fa80c4d1b60d6d6a55ee364c6a2bddc2001
Matthew Rutgers <w0858703@myscc.ca>
2024-09-12 19:06:23 -0400

    Completed Assignment 1 - WEB 307
    -Edited the html
    -Added variables inside .ts, and used them in .html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Matthew-Rutgers-Still-Learning-Angular';
+  myName = "Matthew Rutgers";
+  myAge = 83;
 }
