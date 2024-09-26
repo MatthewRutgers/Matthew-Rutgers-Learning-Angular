02f43d913ce8404d5e59e8af9e073cab35d4854f
Matthew Rutgers <MR244@myscc.ca>
2024-09-26 19:19:08 -0400

    Lab Submission
    -I dont think anything changed idk

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,11 +16,11 @@ import {NgForOf, NgStyle} from "@angular/common";
 })
 export class ProductListComponent {
   productList: Product[] = [
-    {id: 1, itemName: 'Paper Paperclip', madeFrom: 'Paper', desc: 'A paper clip from paper for paper to hold paper', inStock: true, imgPath: "../assets/images/paperpaperclip.png"},
-    {id: 2, itemName: 'Paperless Bread Tag', madeFrom: 'Paperless Cardboard (Plastic)', desc: '1 turtle at a time', inStock: true , imgPath: "../assets/images/paperlessbreadtag.png"},
-    {id: 3, itemName: 'Transparent Keychain', madeFrom: 'Paper Bags', desc: 'No hazards around, only a vision of Jesus', inStock: true, imgPath: "../assets/images/transparentkeychain.png"},
-    {id: 4, itemName: 'Paper Shoelace', madeFrom: 'Paper', desc: 'Too reliable, do not pull', inStock: false, imgPath: "../assets/images/papershoelace.png"},
-    {id: 5, itemName: 'Bouncy Knife', madeFrom: 'Uranium', desc: 'Wear safety glasses and a jock', inStock: true, imgPath: "../assets/images/bouncyknife.png"},
-    {id: 6, itemName: 'Waiting for creativity', madeFrom: 'Asbestos', desc: 'I hope its good', inStock: false, imgPath: "../assets/images/thinking.png"}
+    {id: 1, itemName: 'Paper Paperclip', madeFrom: 'Paper', desc: 'A paper clip from paper for paper to hold paper', inStock: true, imgPath: "src/assets/Cat03.jpg"},
+    {id: 2, itemName: 'Paperless Bread Tag', madeFrom: 'Paperless Cardboard (Plastic)', desc: '1 turtle at a time', inStock: true , imgPath: "../assets/paperlessbreadtag.png"},
+    {id: 3, itemName: 'Transparent Keychain', madeFrom: 'Paper Bags', desc: 'No hazards around, only a vision of Jesus', inStock: true, imgPath: "../assets/transparentkeychain.png"},
+    {id: 4, itemName: 'Paper Shoelace', madeFrom: 'Paper', desc: 'Too reliable, do not pull', inStock: false, imgPath: "../assets/papershoelace.png"},
+    {id: 5, itemName: 'Bouncy Knife', madeFrom: 'Uranium', desc: 'Wear safety glasses and a jock', inStock: true, imgPath: "../assets/bouncyknife.png"},
+    {id: 6, itemName: 'Waiting for creativity', madeFrom: 'Asbestos', desc: 'I hope its good', inStock: false, imgPath: "../assets/thinking.png"}
   ];
 }
