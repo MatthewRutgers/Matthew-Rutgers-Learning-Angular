96c925d446e8020132dee6122665ad8e52433ab2
Matthew Rutgers <MR244@myscc.ca>
2024-10-10 12:31:13 -0400

    Maybe done?
    -I think I already have the stuff asking for in my program but idk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {NgForOf, NgIf} from "@angular/common";
 import {Product} from "./models/product";
 import {ProductListComponent} from "./product-list/product-list.component";
@@ -7,7 +7,7 @@ import {ProductListComponent} from "./product-list/product-list.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf, ProductListComponent],
+  imports: [RouterOutlet, NgForOf, NgIf, ProductListComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {Routes} from "@angular/router";
+import {provideRouter, Routes} from "@angular/router";
 import {ProductListComponent} from "./app/product-list/product-list.component";
 import {ProductListItemComponent} from "./app/product-list-item/product-list-item.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
 const routes: Routes = [
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'products', component: ProductListComponent},
+  {path: 'products/:id', component: ProductListItemComponent},
+  {path: 'modify-product', component: ModifyListItemComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
-]
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
