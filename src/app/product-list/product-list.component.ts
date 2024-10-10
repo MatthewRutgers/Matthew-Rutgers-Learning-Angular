cfed2957e68375ca5fd7b0be20cb0cbe9b61c4f4
Matthew Rutgers <MR244@myscc.ca>
2024-10-10 10:24:30 -0400

    Started Assignment 4

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,7 +16,7 @@ import {NgForOf, NgStyle} from "@angular/common";
 })
 export class ProductListComponent {
   productList: Product[] = [
-    {id: 1, itemName: 'Paper Paperclip', madeFrom: 'Paper', desc: 'A paper clip from paper for paper to hold paper', inStock: true, imgPath: "/assets/Cat03.jpg"},
+    {id: 1, itemName: 'Paper Paperclip', madeFrom: 'Paper', desc: 'A paper clip from paper for paper to hold paper', inStock: true, imgPath: "/assets/paperpaperclip.png"},
     {id: 2, itemName: 'Paperless Bread Tag', madeFrom: 'Paperless Cardboard (Plastic)', desc: '1 turtle at a time', inStock: true , imgPath: "/assets/paperlessbreadtag.png"},
     {id: 3, itemName: 'Transparent Keychain', madeFrom: 'Paper Bags', desc: 'No hazards around, only a vision of Jesus', inStock: true, imgPath: "/assets/transparentkeychain.png"},
     {id: 4, itemName: 'Paper Shoelace', madeFrom: 'Paper', desc: 'Too reliable, do not pull', inStock: false, imgPath: "/assets/papershoelace.png"},
