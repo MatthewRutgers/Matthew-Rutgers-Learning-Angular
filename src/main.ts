914b4c083b0542f1b6f285e089ab7298b09e3358
Matthew Rutgers <MR244@myscc.ca>
2024-10-10 12:10:21 -0400

    Defined a list of routes
    -Created an empty array in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {Routes} from "@angular/router";
+import {ProductListComponent} from "./app/product-list/product-list.component";
+import {ProductListItemComponent} from "./app/product-list-item/product-list-item.component";
+
+const routes: Routes = [
+
+]
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
